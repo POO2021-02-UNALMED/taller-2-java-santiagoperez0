2001b33bfcde70196d8ead9b64a185d218711294
santiagoperez0 <jperezpi@unal.edu.co>
2021-10-19 23:45:46 -0500

    sss

--- src/test/java/test/TestAtributos.java
+++ src/test/java/test/TestAtributos.java
@@ -8,32 +8,32 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class TestAtributos {
-	
+
 	@Test
-   	public void testAtributosMotor(){
-		
+	public void testAtributosMotor(){
+
 		Motor m = new Motor();
 		m.numeroCilindros = 4;
 		m.registro = 142;
 		m.tipo = "electrico";
-		
+
 		assertTrue(true, "Hay un problema con los atributos de la Clase Motor");
-    }
-	
+	}
+
 	@Test
-   	public void testAtributosAsiento(){
-		
+	public void testAtributosAsiento(){
+
 		Asiento a = new Asiento();
 		a.color = "blanco";
 		a.precio = 5000;
 		a.registro = 435;
-		
+
 		assertTrue(true, "Hay un problema con los atributos de la Clase Asiento");
-    }
-	
+	}
+
 	@Test
-   	public void testAtributosAuto(){
-		
+	public void testAtributosAuto(){
+
 		Auto v = new Auto();
 		v.asientos = new Asiento[5];
 		v.marca = "tesla";
@@ -41,8 +41,8 @@ public class TestAtributos {
 		v.motor = new Motor();
 		v.precio = 33000;
 		v.registro = 341;
-		
+
 		assertTrue(true, "Hay un problema con los atributos de la Clase Auto");
-    }
+	}
 
 }
--- src/test/java/test/TestMetodos.java
+++ src/test/java/test/TestMetodos.java
@@ -10,87 +10,87 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class TestMetodos {
-	
+
 	@Test
-   	public void testMetodoCambiarColor(){
-		
+	public void testMetodoCambiarColor(){
+
 		Asiento a1 = new Asiento();
 		Asiento a2 = new Asiento();
-		
+
 		a1.color = "blanco";
 		a2.color = "blanco";
-		
+
 		a1.cambiarColor("naranja");
 		a2.cambiarColor("verde");
-		
+
 		boolean ok = false;
-		
-		if(a1.color.equals("blanco") && 
+
+		if(a1.color.equals("blanco") &&
 				a2.color.equals("verde")) {
 			ok = true;
 		}
 		assertTrue(ok, "Hay un problema con el metodo cambiarColor y sus restricciones de la clase Asiento");
-    }
-	
+	}
+
 	@Test
-   	public void testMetodoCambiarRegistro(){
-		
+	public void testMetodoCambiarRegistro(){
+
 		Motor m = new Motor();
-		
+
 		m.registro = 123;
 		m.cambiarRegistro(423);
-		
+
 		boolean ok = false;
-		
+
 		if(m.registro == 423) {
 			ok = true;
 		}
 
 		assertTrue(ok, "Hay un problema con el metodo cambiarRegistro y sus restricciones de la clase Motor");
-    }
-	
+	}
+
 	@Test
-   	public void testMetodoAsignarTipo(){
-		
+	public void testMetodoAsignarTipo(){
+
 		Motor m1 = new Motor();
 		Motor m2 = new Motor();
 		m1.tipo = "normal";
 		m2.tipo = "normal";
-		
+
 		m1.asignarTipo("hibrido");
 		m2.asignarTipo("electrico");
 		boolean ok = false;
-		
-		if(m1.tipo.equals("normal") && 
+
+		if(m1.tipo.equals("normal") &&
 				m2.tipo.equals("electrico")) {
 			ok = true;
 		}
 
 		assertTrue(ok, "Hay un problema con el metodo asignarTipo y sus restricciones de la clase Motor");
-    }
-	
+	}
+
 	@Test
-   	public void testMetodoCantidadAsientos(){
-		
+	public void testMetodoCantidadAsientos(){
+
 		Auto a = new Auto();
-		
+
 		a.asientos = new Asiento[5];
-		
+
 		a.asientos[0] = new Asiento();
 		a.asientos[4] = new Asiento();
 
 		boolean ok = false;
-		
+
 		if(a.cantidadAsientos() == 2) {
 			ok = true;
 		}
 
 		assertTrue(ok, "Hay un problema con el metodo cantidadAsientos y sus restricciones de la clase Auto");
-    }
-	
+	}
+
 	@Test
-   	public void testMetodoVerificarIntegridad(){
-		
+	public void testMetodoVerificarIntegridad(){
+
 		Auto a = new Auto();
 		Motor m = new Motor();
 		a.registro = 32;
@@ -101,8 +101,8 @@ public class TestMetodos {
 		a.asientos[0].registro = 32;
 		a.asientos[4] = new Asiento();
 		a.asientos[4].registro = 32;
-		
-		
+
+
 		Auto a2 = new Auto();
 		Motor m2 = new Motor();
 		a2.registro = 32;
@@ -115,13 +115,13 @@ public class TestMetodos {
 		a2.asientos[4].registro = 32;
 
 		boolean ok = false;
-		
-		if(a.verificarIntegridad().equals("Auto original") && 
+
+		if(a.verificarIntegridad().equals("Auto original") &&
 				a2.verificarIntegridad().equals("Las piezas no son originales")) {
 			ok = true;
 		}
 
 		assertTrue(ok, "Hay un problema con el metodo verificarIntegridad y sus restricciones de la clase Auto");
-    }
+	}
 
 }
